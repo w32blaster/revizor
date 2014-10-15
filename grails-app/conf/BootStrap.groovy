f6f996ac172063d28e09dd256819170a75251e38
Ilja Hämäläinen <ilja.hamalainen@gmail.com>
2014-10-15 09:10:50 +0100

    Insert initial data on an empty database

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,20 @@
+import com.revizor.Role
+import com.revizor.User
+
 class BootStrap {
 
     def init = { servletContext ->
+
+        if (!User.count()) {
+            User user = new User(
+                    email: 'admin@admin.com',
+                    username: 'admin',
+                    password: 'admin123',
+                    role: Role.ADMIN)
+            user.save(failOnError: true)
+        }
     }
+
     def destroy = {
     }
 }
