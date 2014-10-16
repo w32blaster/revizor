fa2d2c9d240b1e820ddf00295bfca5252eb9828e
w32blaster <ilja.hamalainen@gmail.com>
2014-10-16 22:09:36 +0100

    Create a default 'admin' user in case if the database is empty

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,27 @@
+import com.revizor.Role
+import com.revizor.User
+import com.revizor.utils.Constants
+
 class BootStrap {
 
     def init = { servletContext ->
+
+        def repoDir = new File(Constants.LOCAL_REPO_PATH)
+        if (!repoDir.exists()) repoDir.mkdirs()
+
+        println "Checking the user"
+        if (User.all.isEmpty()) {
+
+            //insert some default user
+            new User(
+                    email: 'admin@admin.com',
+                    username: 'admin',
+                    password: 'admin123',
+                    role: Role.ADMIN
+            ).save(failOnError: true)
+        }
     }
+
     def destroy = {
     }
 }
